1f095f51a3efefff11a340760e6ef9da48ac7070
Commish1977 <castoldi21@gmail.com>
2015-04-10 23:57:39 -0400

    PNG and R files for Week 1 Assignment

---
+++ plot1.R
@@ -0,0 +1,20 @@
+## Plot 1 R File per the Week 1 assignment
+setwd("R")
+library(data.table)
+
+## read in dataset
+  dataset<-read.table("data/household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors=FALSE)
+
+## Key on Data and reformat
+  testset<-data.table(dataset,key="Date")
+  testset<-testset[,Date:=as.Date(Date,"%d/%m/%Y")]
+
+## subset the data for this exercise
+  finalset<-subset(testset, Date>=as.Date("2007-02-01") & Date<=as.Date("2007-02-02"))
+
+## Plot Histogram
+  hist(as.numeric(finalset$Global_active_power),main="Global Active Power",col="red",xlab="Global Active Power (kilowatts)")
+
+## Save to PNG file
+  dev.copy(png,file="plot1.png")
+  dev.off()
